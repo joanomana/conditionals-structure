7712fb66295f049e693b03be1041f2a9a5d0f69a
joanomana <joansebastianomasu@gmail.com>
2024-10-30 18:52:30 -0500

    Solucion ejercicio calculadora

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
-print("Exercise 6")
+print("Exercise 7")
+print()
+print("This program is a basic calculator")
+print()
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
 
-char = input("Enter a character: ")
-if char.isdigit():
-    print("The character is a number.")
-elif char.isalpha():
-    print("The character is a letter.")
-else:
-    print("The character is a special character.")
-
+def calculator():
+    print("1. Addition = +")
+    print("2. Subtraction = -")
+    print("3. Multiplication = *")
+    print("4. Division = /")
+    print("5. elevation = **")
+    print("6. Exit = 6")
+    choice = str(input("Enter the operation number: "))
+    if choice == "+":
+        print("The result is: ", num1 + num2)
+    elif choice == "-":
+        print("The result is: ", num1 - num2)
+    elif choice == "*":
+        print("The result is: ", num1 * num2)
+    elif choice == "/":
+        print("The result is: ", num1 / num2)
+    elif choice == "**":
+        print("The result is: ", num1 ** num2)
+    elif choice == "6":
+        print("Goodbye!")
+    else:
+        print("Invalid choice")
+calculator()
