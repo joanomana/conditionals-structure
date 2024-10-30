f299e329daa94237925100206c0b35aefadd8e47
joanomana <joansebastianomasu@gmail.com>
2024-10-30 12:12:30 -0500

    Solucion numero es par

---
+++ exercise1.py
@@ -0,0 +1,12 @@
+print("Exercise 1")
+
+number = int(input("Enter a number: "))
+
+if number % 2 == 0:
+    print("The number is even.")
+elif number %5==0:
+    print("The number is divisible by 5.")
+else: print("The number is odd.")
+
+
+
