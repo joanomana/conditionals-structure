4858beb6d6ba76fea78b1c6962df7651e1e97dc8
joanomana <joansebastianomasu@gmail.com>
2024-10-30 19:02:06 -0500

    Solucion ejercicio edad

--- main.py
+++ main.py
@@ -1,30 +1,17 @@
-print("Exercise 7")
+from time import localtime
+print("Exercise 8")
 print()
-print("This program is a basic calculator")
-print()
-num1 = int(input("Enter the first number: "))
-num2 = int(input("Enter the second number: "))
 
-def calculator():
-    print("1. Addition = +")
-    print("2. Subtraction = -")
-    print("3. Multiplication = *")
-    print("4. Division = /")
-    print("5. elevation = **")
-    print("6. Exit = 6")
-    choice = str(input("Enter the operation number: "))
-    if choice == "+":
-        print("The result is: ", num1 + num2)
-    elif choice == "-":
-        print("The result is: ", num1 - num2)
-    elif choice == "*":
-        print("The result is: ", num1 * num2)
-    elif choice == "/":
-        print("The result is: ", num1 / num2)
-    elif choice == "**":
-        print("The result is: ", num1 ** num2)
-    elif choice == "6":
-        print("Goodbye!")
-    else:
-        print("Invalid choice")
-calculator()
+print("This program give your age")
+print()
+print("Enter your birthday date")
+day = int(input("Day: "))
+month = int(input("Month: "))
+year = int(input("Year: "))
+print()
+birthday = (day, month, year)
+t = localtime()
+age = localtime().tm_year - birthday[2]
+if (month, day) > (t.tm_mon, t.tm_mday):
+    age -= 1
+print(f"You are {age} years old.")
