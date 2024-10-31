bcc6b3c946d6a9184ec164372a340af0f7fbbc08
joanomana <joansebastianomasu@gmail.com>
2024-10-31 11:40:56 -0500

    Solucion ejercicio tennis

--- main.py
+++ main.py
@@ -1,17 +1,11 @@
-from time import localtime
-print("Exercise 8")
-print()
+player1 = int(input("Games won by A: "))
+player2 = int(input("Games won by B: "))
 
-print("This program give your age")
-print()
-print("Enter your birthday date")
-day = int(input("Day: "))
-month = int(input("Month: "))
-year = int(input("Year: "))
-print()
-birthday = (day, month, year)
-t = localtime()
-age = localtime().tm_year - birthday[2]
-if (month, day) > (t.tm_mon, t.tm_mday):
-    age -= 1
-print(f"You are {age} years old.")
+if ((player1 >= 6 and player1<=7) and player1 - player2 >= 2) or (player1 == 7 and player2 > 5)or (player1 == 7 and player2 == 6):
+    print("A won the set")
+elif ((player2 >= 6 and player2<=7) and player2 - player1 >= 2)and (player2 == 7 and player1 >= 5) or (player2 == 7 and player1 == 6):
+    print("B won the set")
+elif (player1 > 7 or player2 > 7) or (player1 == 7 and player2 < 6) or (player2 == 7 and player1 <6):
+    print("Invalid result")
+else:
+    print("The set is not over yet")
